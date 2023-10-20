1584c812a45e81ec1d3ada3b0bda7388d08e20bc
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2023-10-20 17:15:32 -0400

    finished queries

--- queries.sh
+++ queries.sh
@@ -26,16 +26,17 @@ echo -e "\nNumber of games where the winning team scored more than two goals:"
 echo "$($PSQL "SELECT COUNT(*) FROM games WHERE winner_goals > 2")"
 
 echo -e "\nWinner of the 2018 tournament team name:"
-echo "$($PSQL "SELECT name FROM teams where team_id IN (SELECT winner_id FROM games WHERE year=2018 AND round='Final')")"
+echo "$($PSQL "SELECT name FROM teams WHERE team_id IN (SELECT winner_id FROM games WHERE year=2018 AND round='Final')")"
 
 echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
-echo "$($PSQL "SELECT name FROM teams where team_id IN (SELECT winner_id FROM games WHERE year=2014 AND round='Eighth-Final')")"
+echo "$($PSQL "SELECT name FROM teams WHERE team_id IN (SELECT winner_id FROM games WHERE year=2014 AND round='Eighth-Final')
+OR team_id IN (SELECT opponent_id FROM games WHERE year=2014 AND round='Eighth-Final') ORDER BY name")"
 
 echo -e "\nList of unique winning team names in the whole data set:"
-echo
+echo "$($PSQL "SELECT name FROM teams WHERE team_id IN (SELECT winner_id FROM games) ORDER BY name")"
 
 echo -e "\nYear and team name of all the champions:"
-echo
+echo "$($PSQL "SELECT teams.name, games.year FROM teams INNER JOIN games ON teams.team_id = games.winner_id WHERE games.round='Final'")"
 
 echo -e "\nList of teams that start with 'Co':"
-echo
+echo "$($PSQL "SELECT name FROM teams WHERE name LIKE 'Co%'")"
