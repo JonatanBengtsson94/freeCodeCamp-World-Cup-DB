71dc86cd7e830bace3554a30c75023a6df370a1c
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2023-10-20 07:33:29 -0400

    insert into teams fixed

--- insert_data.sh
+++ insert_data.sh
@@ -8,21 +8,28 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+
 # Truncate table
 $PSQL " TRUNCATE TABLE teams, games"
 # Read lines in csv
 while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
 do
-  # Get team ID
-  TEAMID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
-  # If team not in table
-  if [ -z "$TEAMID" ]
+  # Get id of winner team
+  WINNERID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+  # If team not in table insert the team
+  if [ -z "$WINNERID" ]
   then
     $PSQL " INSERT INTO teams(name) VALUES('$WINNER')"
+    echo inserted $WINNER into teams
   fi
-  echo $TEAMID
-  echo $WINNER
-  echo $OPPONENT
+  # Get id of opposing team
+  OPPONENTID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+  # If team not in table insert the team
+  if [ -z "$OPPONENTID" ]
+  then
+    $PSQL " INSERT INTO teams(name) VALUES('$OPPONENT')"
+    echo inserted $OPPONENT into teams
+  fi
   echo $YEAR
   echo $ROUND
   echo $WINNER_GOALS
